8ce4e32eccde8ecd9fe642a33f9dd69b6493b17d
Alisamavat <alisamavat.dev@gmail.com>
2026-06-05 17:58:34 +0330

    part 7

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Footer from "./(marketing)/_components/Footer";
 import Header from "./(marketing)/_components/Header";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>
       <main>
         <Header/>
+        <Footer/>
       </main>
     </div>
   );
